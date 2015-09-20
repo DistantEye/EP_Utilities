f2380688e5a5c0681e141120ad06b81dac9dbc71
DistantEye <WatchfulDistantEye@gmail.com>
2015-09-19 23:03:26 -0400

    Reworked directive building slightly

--- EclipsePhaseLifePath/src/com/github/distanteye/ep_utils/commands/directives/DirectiveBuilder.java
+++ EclipsePhaseLifePath/src/com/github/distanteye/ep_utils/commands/directives/DirectiveBuilder.java
@@ -15,49 +15,48 @@ public class DirectiveBuilder {
 	/**
 	 * Takes in input string, and runs it against list of possible commands,
 	 * returning the appropriate Directive subclass that best matches
-	 * @param input Effects string matching a SINGLE valid Directive. 
-	 * 			Will throw errors if multiple or unknown directive provided
+	 * @param input Effects string containing at least one valid directive will only match/return the first found
 	 * @return Directive object matching the input string
 	 */
 	public static Directive getDirective(String input)
 	{
 		String lcEffect = Directive.getCommandName(input).toLowerCase();
 		
-		if (Utils.splitCommands(input, ";").length > 1)
+		// structured so only the first top level directive is returned
+		String insides = Utils.returnStringInParen(input, input.indexOf(lcEffect));
+		String inputMod = lcEffect + "(" + insides + ")";
+		
+		if (lcEffect.startsWith("concat"))
 		{
-			throw new IllegalArgumentException("Can only process a single Directive");
-		}
-		else if (lcEffect.startsWith("concat"))
-		{
-			return new ConcatDirective(input);
+			return new ConcatDirective(inputMod);
 		}
 		else if (lcEffect.startsWith("getRand"))
 		{
-			return new GetRandDirective(input);
+			return new GetRandDirective(inputMod);
 		}
 		else if (lcEffect.startsWith("getVar"))
 		{
-			return new GetVarDirective(input);
+			return new GetVarDirective(inputMod);
 		}
 		else if (lcEffect.startsWith("rollDice"))
 		{
-			return new RollDiceDirective(input);
+			return new RollDiceDirective(inputMod);
 		}
 		else if (lcEffect.startsWith("simpRollDice"))
 		{
-			return new SimpRollDiceDirective(input);
+			return new SimpRollDiceDirective(inputMod);
 		}
 		else if (lcEffect.startsWith("add"))
 		{
-			return new AddDirective(input);
+			return new AddDirective(inputMod);
 		}
 		else if (lcEffect.startsWith("mult"))
 		{
-			return new MultDirective(input);
+			return new MultDirective(inputMod);
 		}
 		else
 		{
-			throw new IllegalArgumentException("Poorly formated effect " + input);
+			throw new IllegalArgumentException("Poorly formated effect " + inputMod);
 		}
 	}
 
